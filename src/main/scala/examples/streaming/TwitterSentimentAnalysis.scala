ed55b7022ccb06c8d4eed8dce1840dd2c1625605
mafernandez-stratio <mafernandez@stratio.com>
2019-11-29 01:52:15 +0100

    Improvements

--- src/main/scala/examples/streaming/TwitterSentimentAnalysis.scala
+++ src/main/scala/examples/streaming/TwitterSentimentAnalysis.scala
@@ -1,6 +1,10 @@
 package examples.streaming
 
-import org.apache.log4j.{Level, Logger}
+import java.io.FileInputStream
+import java.util.Properties
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.SparkConf
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.{SaveMode, SparkSession}
@@ -9,25 +13,29 @@ import org.apache.spark.streaming.twitter.TwitterUtils
 
 object TwitterSentimentAnalysis extends App with Logging {
 
-  val consumerKey = "***"
-  val consumerSecret = "***"
-  val accessToken = "***"
-  val accessTokenSecret = "***"
-
   val filters: Array[String] = Array.empty[String]
+
+  val twitterCredentials = new Properties()
+  twitterCredentials.load(new FileInputStream("/Users/miguelangelfernandezdiaz/workspace/twitter.properties"))
 
   // Set the system properties so that Twitter4j library used by Twitter stream
   // can use them to generate OAuth credentials
-  System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
-  System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
-  System.setProperty("twitter4j.oauth.accessToken", accessToken)
-  System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+  System.setProperty("twitter4j.oauth.consumerKey", twitterCredentials.getProperty("twitter-source.consumerKey"))
+  System.setProperty("twitter4j.oauth.consumerSecret", twitterCredentials.getProperty("twitter-source.consumerSecret"))
+  System.setProperty("twitter4j.oauth.accessToken", twitterCredentials.getProperty("twitter-source.token"))
+  System.setProperty("twitter4j.oauth.accessTokenSecret", twitterCredentials.getProperty("twitter-source.tokenSecret"))
 
   val sparkConf = new SparkConf().setMaster("local[*]").setAppName("TwitterSentimentAnalysis")
 
   val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
 
-  val ssc = new StreamingContext(sparkSession.sparkContext, Seconds(10))
+  val hadoopCfg = new Configuration()
+  hadoopCfg.set("fs.defaultFS", "hdfs://localhost:9000")
+  val fs = FileSystem.newInstance(hadoopCfg)
+  fs.delete(new Path("/tmp/streaming/"), true)
+
+  println(" >>> Starting Streaming context")
+  val ssc = new StreamingContext(sparkSession.sparkContext, Seconds(60))
   val stream = TwitterUtils.createStream(ssc, None, filters)
 
   val tweets = stream.filter(_.getHashtagEntities.nonEmpty).map{ status =>
@@ -35,13 +43,10 @@ object TwitterSentimentAnalysis extends App with Logging {
     (source.drop(source.indexOf(">")).stripPrefix(">").stripSuffix("</a>"), status.getHashtagEntities.map(_.getText.toLowerCase), status.getText)
   }
 
-  //val keywords = sparkSession.read.parquet("file:///tmp/keywords.csv").select("word").collect().map(_.getString(0))
-  //val test = sparkSession.read.parquet("file:///tmp/keywords.csv").select("word")
-  val keywords = Array("global","warming","pollution","earth","temperature","increase","weather","change","biosphere",
-    "hydrosphere","lithosphere","solar","radiation","ice","core","fossil","fuel","depolarization","emissions",
-    "climate","co2","air","quality","dust","carbondioxide","greenhouse","ozone","methane","sealevel","sea","level",
-    "TheApprentice","MCCelebrity","LIVNAP","news", "MasterChefCelebrity", "istandwithmelissa", "bigdata", "spark",
-    "streaming", "kafka", "usa").map(_.toLowerCase)
+  val jdbcProperties = new Properties()
+  jdbcProperties.setProperty("user","postgres")
+  val pgHashtags = sparkSession.read.jdbc("jdbc:postgresql://localhost:5432/postgres", "public.hashtags", jdbcProperties)
+  val keywords = pgHashtags.collect().map(_.getString(0).toLowerCase)
 
   val relevantTweets = tweets.filter{ tweet =>
     tweet._2.intersect(keywords).length > 0
@@ -50,15 +55,27 @@ object TwitterSentimentAnalysis extends App with Logging {
   val iphoneTweets = relevantTweets.filter(_._1.toLowerCase.contains("iphone"))
   iphoneTweets.foreachRDD{ rdd =>
     import sparkSession.implicits._
-    logInfo(s"Saving ${rdd.count()} tweets from iphone sources")
-    rdd.toDF("source", "hashtags", "text").write.mode(SaveMode.Append).csv("/tmp/test/iphone")
+    println(s" >>> Saving ${rdd.count()} tweets from iphone sources")
+    rdd.toDF("source", "hashtags", "text").write.mode(SaveMode.Append).parquet("hdfs://localhost:9000/tmp/streaming/iphone")
+
+    val df = sparkSession.read.parquet("hdfs://localhost:9000/tmp/streaming/iphone")
+    println(s" >>> Total tweets from iphone sources: ${df.count()}")
+    println(s" >>> Tweets from iphones:${System.lineSeparator}${df.collect().map{ row =>
+      s"${row.getString(0)} | ${row.getString(1)} | ${row.getString(2)}"
+    }.mkString(System.lineSeparator())}")
   }
 
   val otherSourcesTweets = relevantTweets.filter(!_._1.toLowerCase.contains("iphone"))
   otherSourcesTweets.foreachRDD{ rdd =>
     import sparkSession.implicits._
-    logInfo(s"Saving ${rdd.count()} tweets from other sources")
-    rdd.toDF("source", "hashtags", "text").write.mode(SaveMode.Append).csv("/tmp/test/other")
+    println(s" >>> Saving ${rdd.count()} tweets from other sources")
+    rdd.toDF("source", "hashtags", "text").write.mode(SaveMode.Append).parquet("hdfs://localhost:9000/tmp/streaming/other")
+
+    val df = sparkSession.read.parquet("hdfs://localhost:9000/tmp/streaming/other")
+    println(s" >>> Total tweets from other sources: ${df.count()}")
+    println(s" >>> Tweets from others:${System.lineSeparator}${df.collect().map{ row =>
+      s"${row.getString(0)} | ${row.getString(1)} | ${row.getString(2)}"
+    }.mkString(System.lineSeparator())}")
   }
 
   ssc.start()
