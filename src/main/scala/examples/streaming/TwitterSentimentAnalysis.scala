40e1b6b9c690b5228c95e79164cba767d38cb1ea
mafernandez-stratio <mafernandez@stratio.com>
2020-03-13 12:58:12 +0100

    Improvements in Streaming

--- src/main/scala/examples/streaming/TwitterSentimentAnalysis.scala
+++ src/main/scala/examples/streaming/TwitterSentimentAnalysis.scala
@@ -5,13 +5,19 @@ import java.util.Properties
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
-import org.apache.spark.internal.Logging
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.twitter.TwitterUtils
 
-object TwitterSentimentAnalysis extends App with Logging {
+object TwitterSentimentAnalysis extends App {
+
+  val rootLogger = Logger.getRootLogger()
+
+  rootLogger.setLevel(Level.ERROR)
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
 
   val filters: Array[String] = Array.empty[String]
 
@@ -25,7 +31,8 @@ object TwitterSentimentAnalysis extends App with Logging {
   System.setProperty("twitter4j.oauth.accessToken", twitterCredentials.getProperty("twitter-source.token"))
   System.setProperty("twitter4j.oauth.accessTokenSecret", twitterCredentials.getProperty("twitter-source.tokenSecret"))
 
-  val sparkConf = new SparkConf().setMaster("local[*]").setAppName("TwitterSentimentAnalysis")
+  //val sparkConf = new SparkConf().setMaster("local[*]").setAppName("TwitterSentimentAnalysis")
+  val sparkConf = new SparkConf().setAppName("TwitterSentimentAnalysis")
 
   val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
 
@@ -34,9 +41,11 @@ object TwitterSentimentAnalysis extends App with Logging {
   val fs = FileSystem.newInstance(hadoopCfg)
   fs.delete(new Path("/tmp/streaming/"), true)
 
-  println(" >>> Starting Streaming context")
+  rootLogger.error(" >>> Starting Streaming context")
   val ssc = new StreamingContext(sparkSession.sparkContext, Seconds(60))
   val stream = TwitterUtils.createStream(ssc, None, filters)
+
+  rootLogger.error(" >>> New data window")
 
   val tweets = stream.filter(_.getHashtagEntities.nonEmpty).map{ status =>
     val source = status.getSource
@@ -48,6 +57,8 @@ object TwitterSentimentAnalysis extends App with Logging {
   val pgHashtags = sparkSession.read.jdbc("jdbc:postgresql://localhost:5432/postgres", "public.hashtags", jdbcProperties)
   val keywords = pgHashtags.collect().map(_.getString(0).toLowerCase)
 
+  rootLogger.error(s" >>> Looking up tweets with keywords: ${keywords.mkString(",")}")
+
   val relevantTweets = tweets.filter{ tweet =>
     tweet._2.intersect(keywords).length > 0
   }.map(row => (row._1, row._2.mkString(" "), row._3))
@@ -55,12 +66,12 @@ object TwitterSentimentAnalysis extends App with Logging {
   val iphoneTweets = relevantTweets.filter(_._1.toLowerCase.contains("iphone"))
   iphoneTweets.foreachRDD{ rdd =>
     import sparkSession.implicits._
-    println(s" >>> Saving ${rdd.count()} tweets from iphone sources")
+    rootLogger.error(s" >>> Saving ${rdd.count()} tweets from iphone sources")
     rdd.toDF("source", "hashtags", "text").write.mode(SaveMode.Append).parquet("hdfs://localhost:9000/tmp/streaming/iphone")
 
     val df = sparkSession.read.parquet("hdfs://localhost:9000/tmp/streaming/iphone")
-    println(s" >>> Total tweets from iphone sources: ${df.count()}")
-    println(s" >>> Tweets from iphones:${System.lineSeparator}${df.collect().map{ row =>
+    rootLogger.error(s" >>> Total tweets from iphone sources: ${df.count()}")
+    rootLogger.error(s" >>> Tweets from iphones:${System.lineSeparator}${df.collect().map{ row =>
       s"${row.getString(0)} | ${row.getString(1)} | ${row.getString(2)}"
     }.mkString(System.lineSeparator())}")
   }
@@ -68,12 +79,12 @@ object TwitterSentimentAnalysis extends App with Logging {
   val otherSourcesTweets = relevantTweets.filter(!_._1.toLowerCase.contains("iphone"))
   otherSourcesTweets.foreachRDD{ rdd =>
     import sparkSession.implicits._
-    println(s" >>> Saving ${rdd.count()} tweets from other sources")
+    rootLogger.error(s" >>> Saving ${rdd.count()} tweets from other sources")
     rdd.toDF("source", "hashtags", "text").write.mode(SaveMode.Append).parquet("hdfs://localhost:9000/tmp/streaming/other")
 
     val df = sparkSession.read.parquet("hdfs://localhost:9000/tmp/streaming/other")
-    println(s" >>> Total tweets from other sources: ${df.count()}")
-    println(s" >>> Tweets from others:${System.lineSeparator}${df.collect().map{ row =>
+    rootLogger.error(s" >>> Total tweets from other sources: ${df.count()}")
+    rootLogger.error(s" >>> Tweets from others:${System.lineSeparator}${df.collect().map{ row =>
       s"${row.getString(0)} | ${row.getString(1)} | ${row.getString(2)}"
     }.mkString(System.lineSeparator())}")
   }
