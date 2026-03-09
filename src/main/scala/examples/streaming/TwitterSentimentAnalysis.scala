e8c085f6e6f47c5b078706a05e6d09f8455aac56
mafernandez-stratio <mafernandez@stratio.com>
2026-03-09 23:00:07 +0100

    Streaming example adapted to Spark 3.5

--- src/main/scala/examples/streaming/TwitterSentimentAnalysis.scala
+++ src/main/scala/examples/streaming/TwitterSentimentAnalysis.scala
@@ -1,90 +1,120 @@
 package examples.streaming
 
-import java.io.FileInputStream
 import java.util.Properties
 
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, Path}
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.{SaveMode, SparkSession}
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.logging.log4j.LogManager
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.types._
 
 import scala.util.Try
 
-object TwitterSentimentAnalysis extends App {
+object TwitterSentimentAnalysis35 extends App {
 
-  val rootLogger = Logger.getRootLogger()
+  val logger = LogManager.getLogger(getClass)
 
-  rootLogger.setLevel(Level.WARN)
+  val pgURL = Try(args(0)).getOrElse("jdbc:postgresql://localhost:5432/postgres")
+  val kafkaBootstrap = Try(args(1)).getOrElse("localhost:9092")
+  val kafkaTopic = Try(args(2)).getOrElse("tweets")
+  val windowSecs = Try(args(3).toInt).getOrElse(30)
+  val hdfsBasePath = Try(args(4)).getOrElse("hdfs://localhost:8020/tmp/streaming")
 
-  Logger.getLogger("org").setLevel(Level.ERROR)
+  val spark = SparkSession.builder()
+    .appName("TwitterSentimentAnalysis35")
+    .master("local[*]")   // quitar si vas a cluster
+    .getOrCreate()
 
-  val twitterFile = Try(args(0)).getOrElse("/home/crossdata/Descargas/twitter.properties")
-  val pgURL = Try(args(1)).getOrElse("jdbc:postgresql://localhost:5432/postgres")
-  val windowSecs: Int = Try(args(2).toInt).getOrElse(30)
+  import spark.implicits._
 
-  val filters: Array[String] = Array.empty[String]
-  val twitterCredentials = new Properties()
-  twitterCredentials.load(new FileInputStream(twitterFile))
+  logger.warn(s"pgURL=$pgURL")
+  logger.warn(s"kafkaBootstrap=$kafkaBootstrap")
+  logger.warn(s"kafkaTopic=$kafkaTopic")
+  logger.warn(s"windowSecs=$windowSecs")
+  logger.warn(s"hdfsBasePath=$hdfsBasePath")
 
-  // Set the system properties so that Twitter4j library used by Twitter stream
-  // can use them to generate OAuth credentials
-  System.setProperty("twitter4j.oauth.consumerKey", twitterCredentials.getProperty("twitter-source.consumerKey"))
-  System.setProperty("twitter4j.oauth.consumerSecret", twitterCredentials.getProperty("twitter-source.consumerSecret"))
-  System.setProperty("twitter4j.oauth.accessToken", twitterCredentials.getProperty("twitter-source.token"))
-  System.setProperty("twitter4j.oauth.accessTokenSecret", twitterCredentials.getProperty("twitter-source.tokenSecret"))
+  // Esquema esperado en Kafka
+  val tweetSchema = StructType(Seq(
+    StructField("source", StringType, nullable = true),
+    StructField("hashtags", ArrayType(StringType), nullable = true),
+    StructField("text", StringType, nullable = true)
+  ))
 
-  val sparkConf = new SparkConf().setMaster("local[*]").setAppName("TwitterSentimentAnalysis")
-  //val sparkConf = new SparkConf().setAppName("TwitterSentimentAnalysis")
+  // Lectura desde Kafka
+  val kafkaDF = spark.readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", kafkaBootstrap)
+    .option("subscribe", kafkaTopic)
+    .option("startingOffsets", "latest")
+    .load()
 
-  val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+  val tweets = kafkaDF
+    .selectExpr("CAST(value AS STRING) AS json_value")
+    .select(from_json($"json_value", tweetSchema).as("tweet"))
+    .select("tweet.*")
+    .withColumn("hashtags", coalesce($"hashtags", array()))
+    .withColumn("text", coalesce($"text", lit("")))
+    .withColumn("source", coalesce($"source", lit("")))
 
-  val hadoopCfg = new Configuration()
-  hadoopCfg.set("fs.defaultFS", "hdfs://localhost:8020")
-  val fs = FileSystem.newInstance(hadoopCfg)
-  fs.delete(new Path("/tmp/sentiment_analysis/"), true)
+  val query = tweets.writeStream
+    .trigger(Trigger.ProcessingTime(s"${windowSecs} seconds"))
+    .outputMode("append")
+    .foreachBatch { (batchDF: DataFrame, batchId: Long) =>
+      logger.warn(s">>> New batch: $batchId")
 
-  rootLogger.warn(" >>> Starting Streaming context")
-  rootLogger.warn(s"twitterFile=$twitterFile")
-  rootLogger.warn(s"pgURL=$pgURL")
-  val ssc = new StreamingContext(sparkSession.sparkContext, Seconds(windowSecs))
-  val stream = TwitterUtils.createStream(ssc, None, filters)
+      val jdbcProperties = new Properties()
+      jdbcProperties.setProperty("user", "postgres")
+      jdbcProperties.setProperty("password", "postgres")
 
-  stream.foreachRDD{ rdd =>
-    import sparkSession.implicits._
+      val pgHashtags = batchDF.sparkSession.read
+        .jdbc(pgURL, "public.hashtags", jdbcProperties)
 
-    rootLogger.warn(" >>> New data window")
+      val keywords = pgHashtags.collect().map(_.getString(0).toLowerCase).toSeq
 
-    val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
+      logger.warn(s">>> Looking up tweets with keywords: ${keywords.mkString(",")}")
 
-    val jdbcProperties = new Properties()
-    jdbcProperties.setProperty("user", "postgres")
-    jdbcProperties.setProperty("password", "postgres")
-    val pgHashtags = spark.read.jdbc(pgURL, "public.hashtags", jdbcProperties)
-    val keywords = pgHashtags.collect().map(_.getString(0).toLowerCase)
-    rootLogger.warn(s" >>> Looking up tweets with keywords: ${keywords.mkString(",")}")
+      if (keywords.nonEmpty) {
+        val relevantTweets = batchDF
+          .withColumn(
+            "source_clean",
+            when(
+              regexp_extract($"source", ">([^<]+)<", 1) =!= "",
+              regexp_extract($"source", ">([^<]+)<", 1)
+            ).otherwise($"source")
+          )
+          .withColumn("hashtags_lower", transform($"hashtags", h => lower(h)))
+          .filter(size(array_intersect($"hashtags_lower", typedLit(keywords))) > 0)
+          .select(
+            $"source_clean".as("source"),
+            concat_ws(" ", $"hashtags_lower").as("hashtags"),
+            $"text"
+          )
 
-    val relevantTweets = rdd.filter{ event =>
-      event.getHashtagEntities.nonEmpty && event.getHashtagEntities.map(_.getText.toLowerCase).intersect(keywords).length > 0
-    }.map{ status =>
-      val source = status.getSource
-      (source.drop(source.indexOf(">")).stripPrefix(">").stripSuffix("</a>"), status.getHashtagEntities.map(_.getText.toLowerCase).mkString(" "), status.getText)
-    }
-    relevantTweets.cache()
+        relevantTweets.cache()
 
-    val iphoneTweets = relevantTweets.filter(_._1.toLowerCase.contains("iphone"))
-    rootLogger.warn(s" >>> Saving ${iphoneTweets.count()} tweets from iphone sources")
-    iphoneTweets.toDF("source", "hashtags", "text").write.mode(SaveMode.Append).parquet("hdfs://localhost:9000/tmp/streaming/iphone")
+        val iphoneTweets = relevantTweets.filter(lower($"source").contains("iphone"))
+        val iphoneCount = iphoneTweets.count()
+        logger.warn(s">>> Saving $iphoneCount tweets from iphone sources")
 
-    val otherSourcesTweets = relevantTweets.filter(!_._1.toLowerCase.contains("iphone"))
-    rootLogger.warn(s" >>> Saving ${otherSourcesTweets.count()} tweets from other sources")
-    otherSourcesTweets.toDF("source", "hashtags", "text").write.mode(SaveMode.Append).parquet("hdfs://localhost:9000/tmp/streaming/other")
+        iphoneTweets.write
+          .mode(SaveMode.Append)
+          .parquet(s"$hdfsBasePath/iphone")
 
-  }
+        val otherSourcesTweets = relevantTweets.filter(not(lower($"source").contains("iphone")))
+        val otherCount = otherSourcesTweets.count()
+        logger.warn(s">>> Saving $otherCount tweets from other sources")
 
-  ssc.start()
-  ssc.awaitTermination()
+        otherSourcesTweets.write
+          .mode(SaveMode.Append)
+          .parquet(s"$hdfsBasePath/other")
 
+        relevantTweets.unpersist()
+      } else {
+        logger.warn(">>> No keywords found in PostgreSQL table public.hashtags")
+      }
+
+      ()
+    }.start()
+
+  query.awaitTermination()
 }
